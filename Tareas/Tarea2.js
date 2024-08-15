91d2528c3e6f435dff3c75bf33b94b2ff7cbfcce
ksofia05 <karensofializcanotorres@gmail.com>
2024-08-15 17:58:38 -0500

    Añadir Tarea del 14 de Agosto

---
+++ Tareas/Tarea2.js
@@ -0,0 +1,9 @@
+//Nombre letra por Letra
+const NombreLetraxLetra = (nombre) => {
+    let i = 0;
+    while (nombre[i] !== undefined){
+        console.log(nombre[i])
+        i++
+    }
+}
+NombreLetraxLetra("Karen")
